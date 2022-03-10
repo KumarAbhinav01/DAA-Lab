d277714c33380b302dff6f84d06a5c9c57697c2d
KumarAbhinav01 <abhinav.bitd@gmail.com>
2022-03-10 15:35:35 +0530

    Lab 8

--- Lab 8/q2.cpp
+++ Lab 8/q2.cpp
@@ -5,28 +5,21 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int N=0;
+
+void printMatrix(int matrix[][N]);
+
+void WarshallAlg(int graph[][N]) 
 {
+  int matrix[N][N], i, j, k;
 
-    return 0;
-}
-
-#define nV 4
-
-#define INF 999
-
-void printMatrix(int matrix[][nV]);
-
-void floydWarshall(int graph[][nV]) {
-  int matrix[nV][nV], i, j, k;
-
-  for (i = 0; i < nV; i++)
-    for (j = 0; j < nV; j++)
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
       matrix[i][j] = graph[i][j];
 
-  for (k = 0; k < nV; k++) {
-    for (i = 0; i < nV; i++) {
-      for (j = 0; j < nV; j++) {
+  for (k = 0; k < N; k++) {
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
         if (matrix[i][k] + matrix[k][j] < matrix[i][j])
           matrix[i][j] = matrix[i][k] + matrix[k][j];
       }
@@ -35,9 +28,9 @@ void floydWarshall(int graph[][nV]) {
   printMatrix(matrix);
 }
 
-void printMatrix(int matrix[][nV]) {
-  for (int i = 0; i < nV; i++) {
-    for (int j = 0; j < nV; j++) {
+void printMatrix(int matrix[][]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       if (matrix[i][j] == INF)
         printf("%4s", "INF");
       else
@@ -49,11 +42,18 @@ void printMatrix(int matrix[][nV]) {
 
 int main() 
 {
-    int n;
+    int n,i,j;
     cout<<"Enter no. of vertices: ";
     cin>>n;
-    cout
-    int graph[nV][nV] = {{0, 3, INF, 5}, {2, 0, INF, 4}, {INF, 1, 0, INF}, {INF, INF, 2, 0}};
+    N=n;
+    int A[n][n];
+    cout<<"Enter the elements: ";
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+        {
+            cout<<"Element A["<<i<<"]["<<j<<"]";
+            cin>>A[i][j];
+        }
     
-    floydWarshall(graph);
+    WarshallAlg(A);
 }
--- Lab 8/q3.cpp
+++ Lab 8/q3.cpp
@@ -1,9 +1,67 @@
-// WAP to implement Floyd's algorithm of all pair shortest path 
+/* WAP to implement Floyd's Algorithm of all pair shortest path.
+(No. of vertices = 'n') 
+*/
 
 #include<iostream>
 using namespace std;
 
-int main()
+int N=0;
+
+void printMatrix(int matrix[][N]);
+
+void floydAlg(int graph[][N]) 
 {
-    return 0;
+  int matrix[N][N], i, j, k;
+
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      matrix[i][j] = graph[i][j];
+
+  for (k = 0; k < N; k++) {
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+          matrix[i][j] = matrix[i][k] + matrix[k][j];
+      }
+    }
+  }
+  printMatrix(matrix);
+}
+
+void printMatrix(int matrix[][]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (matrix[i][j] == INF)
+        printf("%4s", "INF");
+      else
+        printf("%4d", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main() 
+{
+    int n,i,j;
+    cout<<"Enter no. of vertices: ";
+    cin>>n;
+    N=n;
+    int A[n][n];
+    cout<<"Enter the elements: ";
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+        {
+            if(i==j)
+            {
+                A[i][j]=0;
+                cout<<"Element A["<<i<<"]["<<j<<"] = 0";
+            }
+            else
+            {
+                cout<<"Element A["<<i<<"]["<<j<<"]";
+                cin>>A[i][j];
+            }
+        }
+    
+    floydAlg(A);
 }
